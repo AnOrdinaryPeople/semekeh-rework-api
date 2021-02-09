feafc484a2bb1e3400afa7412aaa57a0cdd55c4e
Muhammad Dafitra Ar-Rasyid <muhamad.odd@gmail.com>
2021-02-09 14:56:24 +0700

    Created api admin homepage

--- routes/web.php
+++ routes/web.php
@@ -40,5 +40,60 @@ $router->group(['prefix' => 'api/auth'], function() use ($router){
 });
 
 $router->group(['prefix' => 'admin', 'middleware' => ['auth']], function() use ($router){
-    //
+	$router->post('{path}/upload', 'HomepageController@upload');
+
+    $router->group(['prefix' => 'homepage'], function() use ($router){
+    	$router->get('/', 'HomepageController@index');
+
+    	$router->group(['prefix' => 'carousel'], function() use ($router){
+    		$router->get('/', 'CarouselController@table');
+    		$router->post('create', 'CarouselController@create');
+    		$router->post('update/{id}', 'CarouselController@update');
+    		$router->delete('delete/{id}', 'CarouselController@delete');
+    	});
+
+    	$router->group(['prefix' => 'video'], function() use ($router){
+    		$router->get('/', 'VideoController@table');
+    		$router->post('create', 'VideoController@create');
+    		$router->post('update/{id}', 'VideoController@update');
+    		$router->post('publish/{id}', 'VideoController@publish');
+    		$router->delete('delete/{id}', 'VideoController@delete');
+    	});
+
+		$router->post('about/update', 'HomepageController@aboutUpdate');
+
+		$router->group(['prefix' => 'alumni'], function() use ($router){
+    		$router->get('/', 'AlumniController@table');
+    		$router->post('create', 'AlumniController@create');
+    		$router->post('update/{id}', 'AlumniController@update');
+    		$router->post('publish/{id}', 'AlumniController@publish');
+    		$router->delete('delete/{id}', 'AlumniController@delete');
+    	});
+
+    	$router->group(['prefix' => 'company'], function() use ($router){
+    		$router->get('/', 'CompanyController@table');
+    		$router->post('create', 'CompanyController@create');
+    		$router->post('update/{id}', 'CompanyController@update');
+    		$router->delete('delete/{id}', 'CompanyController@delete');
+    	});
+
+    	$router->group(['prefix' => 'social'], function() use ($router){
+    		$router->get('/', 'SocialController@table');
+    		$router->post('create', 'SocialController@create');
+    		$router->post('update/{id}', 'SocialController@update');
+    		$router->delete('delete/{id}', 'SocialController@delete');
+    	});
+
+    	$router->group(['prefix' => 'footer'], function() use ($router){
+    		$router->get('/', 'FooterController@table');
+    		$router->post('create', 'FooterController@create');
+    		$router->post('update/{id}', 'FooterController@update');
+    		$router->delete('delete/{id}', 'FooterController@delete');
+    	});
+
+    	$router->group(['prefix' => 'section'], function() use ($router){
+    		$router->get('/', 'SectionController@table');
+    		$router->post('update/{id}', 'SectionController@update');
+    	});
+    });
 });
